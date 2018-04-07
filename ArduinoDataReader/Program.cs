3b666943cbba8914bf14bd594862b5c14eddcc5b
walid-dev <walid-dev@outlook.com>
2018-04-07 20:16:18 +0100

    c# code

---
+++ ArduinoDataReader/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            SerialPort myport = new SerialPort();
+            myport.BaudRate = 9600;
+            myport.PortName = "COM6"; // choose your COM port 
+            myport.Open();
+
+            while (true)
+            {
+
+                String information = myport.ReadLine();
+                Console.WriteLine(information);
+
+
+            }
+
+        }
+    }
+}
